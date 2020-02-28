13350fbbb525b80b929242c80dff0e55deb57a7b
longzi <891661163@qq.com>
2020-02-28 10:35:05 +0800

    修改

--- src/pages/stateHook.js
+++ src/pages/stateHook.js
@@ -4,12 +4,23 @@ import Back from '../components/back';
 function Main() {
   // 声明一个叫 “count” 的 state 变量。
   const [count, setCount] = useState(0);
+  const [count2, setCount2] = useState(0);
 
   return (
     <div>
       <Back />
-      <p>You clicked {count} times</p>
-      <button onClick={() => setCount(count + 1)}>Click me</button>
+      <div>
+        Count: {count}
+        <button onClick={() => setCount(0)}>Reset</button>
+        <button onClick={() => setCount(prevCount => prevCount - 1)}>-</button>
+        <button onClick={() => setCount(prevCount => prevCount + 1)}>+</button>
+      </div>
+      <div>
+        Count: {count2}
+        <button onClick={() => setCount2(0)}>Reset</button>
+        <button onClick={() => setCount2(count2 - 1)}>-</button>
+        <button onClick={() => setCount2(count2 + 1)}>+</button>
+      </div>
     </div>
   );
 }
